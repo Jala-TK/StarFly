7c0e5d45262a092ba1b45363a8aa5fa7496bf165
Kainan <kainan.ars@gmail.com>
2024-10-10 00:01:19 -0300

    fix: update login with session

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,14 @@ export default auth((req) => {
     const newUrl = new URL('/login', req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
+  if (req.nextUrl.pathname === '/login') {
+    const newUrl = new URL('/', req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
 });
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|registro|login|socket.io).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|registro|socket.io).*)',
   ],
 };
